21cb1aee85eb33d9d1b873ba91ea3e41ece514d3
pengyunge <1339433514@qq.com>
2016-03-02 09:47:43 +0800

    彭云革 4

--- HappyCommunity/HappyCommunity/Classes/Models/NewsModel.m
+++ HappyCommunity/HappyCommunity/Classes/Models/NewsModel.m
@@ -9,5 +9,8 @@
 #import "NewsModel.h"
 
 @implementation NewsModel
-- (void)setValue:(id)value forUndefinedKey:(NSString *)key{}
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key{
+}
+
 @end
